1f7c06a62f25c14d70090961740da4ef0aa1e827
Rockes Git User <proowww@gmail.com>
2023-04-08 22:04:37 +0500

    3 step

---
+++ src/main/java/org/example/model/kek.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+public class kek {
+    private String kkk;
+
+    public String getKkk() {
+        return kkk;
+    }
+
+    public void setKkk(String kkk) {
+        this.kkk = kkk;
+    }
+}
